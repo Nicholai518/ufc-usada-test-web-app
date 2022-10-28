32fe7003b79ac0e7d14e637e9a420d86b59a02e1
Nicholai <n.paz4444@gmail.com>
2022-10-27 23:16:37 -0400

    created skeleton spring boot API for project

---
+++ api/src/main/java/com/nicholaspazienza/ufcusadatestapi/UfcUsadaTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.nicholaspazienza.ufcusadatestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcUsadaTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcUsadaTestApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/nicholaspazienza/ufcusadatestapi/UfcUsadaTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholaspazienza.ufcusadatestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcUsadaTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
